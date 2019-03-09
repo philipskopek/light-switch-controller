566cc2fdf3035df42e4b0565f2812ed9196d1a2c
Phillysko <48116359+Phillysko@users.noreply.github.com>
2019-03-08 23:47:56 -0800

    test code for interrupts

---
+++ code-testing/test.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM) #Set pin number configuration to BCM numbering
+button = 2 #button set to GPIO2
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def button_callback(channel):
+    print("Button was pushed!")
+
+GPIO.add_event_detect(4, GPIO.RISING, callback=toggleState, bouncetime=300)
+
+try:
+    while True:
+        time.sleep(60) #you can put every value of sleep you want here..
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
