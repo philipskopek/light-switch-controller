eed83f837ced717b21a944a1c41c32994a629e45
Phillysko <48116359+Phillysko@users.noreply.github.com>
2019-03-09 00:20:29 -0800

    warnings

--- code-testing/testing.py
+++ code-testing/testing.py
@@ -4,6 +4,7 @@ import time
 GPIO.setmode(GPIO.BCM) #Set pin number configuration to BCM numbering
 button = 2 #button set to GPIO2
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setwarnings(False) # Ignore warning for now
 
 def button_callback(channel):
     print("Button was pushed!")
