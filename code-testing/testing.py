1d47b0fad257d41fcb450574e177404729ca2ab3
Phillysko <48116359+Phillysko@users.noreply.github.com>
2019-03-09 00:26:42 -0800

    s

--- code-testing/testing.py
+++ code-testing/testing.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM) #Set pin number configuration to BCM numbering
-button = 2 #button set to GPIO2
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+ #button set to GPIO2
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setwarnings(False) # Ignore warning for now
 
 def button_callback(channel):
