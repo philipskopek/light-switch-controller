b9ef45b0f2c18d59abd6c19309f83b9c6085d0ef
Phillysko <48116359+Phillysko@users.noreply.github.com>
2019-03-09 23:47:28 -0800

    s

--- code-testing/testing2.py
+++ code-testing/testing2.py
@@ -14,11 +14,11 @@ def button_callback(channel):
     while True:
         if (button_cycle%3 == 0):
             while button_cycle%3 == 0:
-                print('button cycle 1')
+                print('button cycle 0')
 
         elif (button_cycle%3 == 1):
             while (button_cycle%3 == 1):
-                print('button cycle 2')
+                print('button cycle 1')
 
         elif (button_cycle%3 == 2):
             while (button_cycle%3 == 2):
